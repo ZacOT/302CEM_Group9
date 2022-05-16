47ebed6367e29ed87983ce97c837e5c2b39336bb
ZacOT <62330525+ZacOT@users.noreply.github.com>
2022-05-16 11:47:40 +0800

    Added (Sprint 1) Component and Routing
    Added Homepage, Login, Register and Shopping Cart Components as well as relevant URL routing.

--- 302CEM_BS/src/app/app-routing.module.ts
+++ 302CEM_BS/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'homepage', component: HomepageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  { path: 'cart', component: ShoppingCartComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- 302CEM_BS/src/app/app.module.ts
+++ 302CEM_BS/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
